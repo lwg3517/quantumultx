cb64e69dc96d4fdfb66fae5337c448f7bdacb9db
lwg3517 <60285110+lwg3517@users.noreply.github.com>
2021-05-19 17:28:35 +0800

    Update price.js

--- price.js
+++ price.js
@@ -5,5 +5,7 @@ hostname = api.m.jd.com,trade-acs.m.taobao.com, amdc.m.taobao.com
 ^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://raw.githubusercontent.com/JDHelloWorld/jd_price/main/jd_price.js
 
 #淘宝历史比价 (by yichahucha)
-^http://.+/amdc/mobileDispatch,requires-body=1,script-path=https://raw.githubusercontent.com/yichahucha/surge/master/tb_price_lite.js
+^http://.+/amdc/mobileDispatch url script-request-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
+^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.getdetail url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/tb_price.js
+
 
